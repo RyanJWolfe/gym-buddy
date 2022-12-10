b8215df78ab38eafd14daeb1c068ec93e204273c
Ryan Wolfe <xryanwolfe@gmail.com>
2022-12-10 16:02:27 -0700

    Rename home controller to pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get 'home', to: 'home#index'
   # Defines the root path route ("/")
-  root 'home#index'
+  root 'pages#home'
 end

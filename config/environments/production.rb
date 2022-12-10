3be64531fae67bcd2985cadbfa0504deae98021b
Ryan Wolfe <xryanwolfe@gmail.com>
2022-12-10 13:31:04 -0700

    Install devise
    - Add simple alerts
    - Add tweaks to get devise working with turbo
    - Add default mailer host to environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "gym_buddy_production"
 
+  # Set mailer host
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.

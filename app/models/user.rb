8184f0501d54d59559b09ff04f222a24bafeb9a2
Ryan Wolfe <xryanwolfe@gmail.com>
2022-12-17 14:13:59 -0700

    Add username and names to User, allow username or email for Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,35 @@
 class User < ApplicationRecord
+  attr_writer :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
-
   # This access the Relationship object
-  has_many :followed_users,
-           foreign_key: :follower_id,
-           class_name: 'Relationship',
-           dependent: :destroy
-
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Relationship', dependent: :destroy
   # This access the User object through the Relationship object
   has_many :followees, through: :followed_users, dependent: :destroy
-
-  has_many :following_users,
-           foreign_key: :followee_id,
-           class_name: 'Relationship',
-           dependent: :destroy
-
+  # This access the Relationship object
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Relationship', dependent: :destroy
+  # This access the User object through the Relationship object
   has_many :followers, through: :following_users, dependent: :destroy
-
   has_many :routines, dependent: :destroy
   has_many :workouts, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  # only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  # https://github.com/heartcombo/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_17_190512) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -60,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_17_190512) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "workout_sets", force: :cascade do |t|

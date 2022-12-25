f0dd8ac9f2388f57f5a5e69fe85ab4aa5ce07968
Ryan Wolfe <xryanwolfe@gmail.com>
2022-12-24 22:50:59 -0700

    Add exercise params to routine model, controller, and form (partially)

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class RoutinesController < ApplicationController
-  before_action :set_routine, only: %i[ show edit update destroy ]
+  before_action :set_routine, only: %i[show edit update destroy]
 
   # GET /routines or /routines.json
   def index
@@ -7,17 +9,17 @@ class RoutinesController < ApplicationController
   end
 
   # GET /routines/1 or /routines/1.json
-  def show
-  end
+  def show; end
 
   # GET /routines/new
   def new
     @routine = Routine.new
+    @routine.routine_exercises.build
+    @routine.exercises.build
   end
 
   # GET /routines/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /routines or /routines.json
   def create
@@ -25,7 +27,7 @@ class RoutinesController < ApplicationController
 
     respond_to do |format|
       if @routine.save
-        format.html { redirect_to routine_url(@routine), notice: "Routine was successfully created." }
+        format.html { redirect_to routine_url(@routine), notice: 'Routine was successfully created.' }
         format.json { render :show, status: :created, location: @routine }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +40,7 @@ class RoutinesController < ApplicationController
   def update
     respond_to do |format|
       if @routine.update(routine_params)
-        format.html { redirect_to routine_url(@routine), notice: "Routine was successfully updated." }
+        format.html { redirect_to routine_url(@routine), notice: 'Routine was successfully updated.' }
         format.json { render :show, status: :ok, location: @routine }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +54,20 @@ class RoutinesController < ApplicationController
     @routine.destroy
 
     respond_to do |format|
-      format.html { redirect_to routines_url, notice: "Routine was successfully destroyed." }
+      format.html { redirect_to routines_url, notice: 'Routine was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_routine
-      @routine = Routine.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def routine_params
-      params.require(:routine).permit(:user_id, :name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_routine
+    @routine = Routine.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def routine_params
+    params.require(:routine).permit(:name, routine_exercises_attributes: [:order, { exercise_id: [] }])
+  end
 end
--- app/models/routine.rb
+++ app/models/routine.rb
@@ -3,5 +3,7 @@ class Routine < ApplicationRecord
   has_many :workouts
   has_many :routine_exercises
   has_many :exercises, through: :routine_exercises
+
+  accepts_nested_attributes_for :routine_exercises, allow_destroy: true
   # has_and_belongs_to_many :exercises
 end
